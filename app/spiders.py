144a1a247430135002eec4ff4672f9f466b1f494
Ondřej Garncarz <ondrej@garncarz.cz>
2023-12-01 22:11:43 +0100

    Spider: checking for sufficient data when starting requests,
    not when already parsing.

--- app/spiders.py
+++ app/spiders.py
@@ -5,20 +5,23 @@ import scrapy
 from . import models, settings
 
 
+def enough_is_enough():
+    if models.Ad.query.count() > settings.MAX_ITEMS:
+        raise scrapy.exceptions.CloseSpider('Enough items in DB')
+
+
 class AdSpider(scrapy.Spider):
     name = 'ad_spider'
 
     def start_requests(self):
         for page in itertools.count():
+            enough_is_enough()
             yield scrapy.Request(
                 'https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1'
                 f'&per_page=20&page={page}'
             )
 
     def parse(self, response):
-        if models.Ad.query.count() > settings.MAX_ITEMS:
-            raise scrapy.exceptions.CloseSpider('Enough items in DB')
-
         data = response.json()
         for estate in data['_embedded']['estates']:
             yield dict(
